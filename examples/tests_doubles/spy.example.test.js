f46a19971db157eb892ea3c96044929bdbe52c2d
Vinicius Morais Dutra <vinicius.dutra@beadev.net>
2023-08-02 21:24:45 -0300

    testando o hook

--- examples/tests_doubles/spy.example.test.js
+++ examples/tests_doubles/spy.example.test.js
@@ -8,11 +8,24 @@ function logResult(result) {
   console.log(`Result: ${result}`);
 }
 
+
+function getMyProfile(userId) {
+  return {
+    id: userId,
+    name: 'John Doe',
+    email: 'vinicius.dutra@beadev.net',
+    age: '30',
+    address: 'Rua dos Bobos, 0'
+  }
+}
+
 // Fixture Object
 const logResultSpy = jest.spyOn(console, "log");
 
 describe("Teste de soma", () => {
   // Deve retornar a soma correta de dois números e chamar a função logResult
+  
+  
   it(`Given (dado): dois números 5 e 10
       When (quando): a função sum é chamada passando os dois números
       Then (então): deve retornar 15
@@ -26,6 +39,24 @@ describe("Teste de soma", () => {
 
     // Assert
     expect(sut).toBe(15);
+
+    // Check if the logResult function was called with the correct parameter
+    expect(logResultSpy).toHaveBeenCalledWith("Result: 15");
+  });
+
+  it(`Given (dado): que eu sou um usuario
+      When (quando): eu entrar no aplicativo na tela de meu perfil
+      Then (então): eu quero ver meu nome e email
+    `, () => {
+    // Arrange
+    const userId = 5;
+
+    // Act
+    const sut = getMyProfile(userId);
+
+    // Assert
+    expect(sut.email).toBe('vinicius.dutra@beadev.net')
+    expect(sut.name).toBe('John Doe')
 
     // Check if the logResult function was called with the correct parameter
     expect(logResultSpy).toHaveBeenCalledWith("Result: 15");
