960c0ec3c2bc83b25d42721776a1e340991525fd
Vinicius Dutra <dutra_morais@hotmail.com>
2023-08-24 18:42:12 -0300

    Adicionei o Arquivo App.tsx

--- RNTLTraining/App.tsx
+++ RNTLTraining/App.tsx
@@ -3,10 +3,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 type RootStackParamList = {
   Login: undefined;
   Home: {token: string};
+  Profile: {token: string};
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -19,6 +21,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
